214c2ff7e56f3d51459f691b3d3b47618101b386
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-07-16 14:55:09 +0000

    Update `dict`

--- empty.py
+++ empty.py
@@ -38,3 +38,17 @@ trailing = {
 	5,
 	7,
 }
+
+# dict
+
+empty = dict()
+empty = {}
+
+singleton = {"hello": 42}
+
+trailing = {
+	2: "good morning",
+	3: "good afternoon",
+	5: "good evening",
+	7: "good night",
+}
