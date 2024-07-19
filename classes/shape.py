be4b3a76dc6455c9dfd361b058b33ff213d69e10
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-07-19 14:07:21 +0000

    Add magic methods

--- classes/shape.py
+++ classes/shape.py
@@ -10,6 +10,12 @@ class Rectangle:
 		self.length = float(length)
 		self.height = float(height)
 
+	def __add__(self, other: Rectangle) -> Rectangle:
+		return self.add(other)
+
+	def __mul__(self, factor: float | int) -> Rectangle:
+		return self.mul(factor)
+
 	@property
 	def perimeter(self) -> float:
 		return 2 * (self.length + self.height)
