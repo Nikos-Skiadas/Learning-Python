3b7b2e0eb9d63003c5a04739a924338d345cb8ae
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-07-23 14:19:22 +0000

    Fix syntax errors

--- classes/shape.py
+++ classes/shape.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 
+from math import sqrt
+
+
 class Rectangle:
 
 	def __init__(self,
@@ -37,35 +40,33 @@ class Rectangle:
 		return self.length * self.height
 
 
-from math import sqrt
-
 class Isosceles:
 
-    def __init__(self, 
-            side: float | int = 0.,
-            base: float | int = 0.
-        ):
-        
-        self.side = float(side)
-        self.base = float(base)
-    
-    def __repr__(self) -> str:
-    
-    def __add__(self, other: 'Isosceles') -> 'Isosceles':
-        return Isosceles(
-            self.side + other.side,
-            self.base + other.base,
-        )
-    
-    def __mul__(self, factor: float | int) -> 'Isosceles':
-        return Isosceles(
-            self.side * factor,
-            self.base * factor,
-        )
-    
-    def __rmul__(self, factor: float | int) -> 'Isosceles':
-        return self * factor
-    
-    @property
-    def perimeter(self) -> float:
-        return 2 * self.side + self.base
+	def __init__(self,
+		side: float | int = 0.,
+		base: float | int = 0.
+	):
+		self.side = float(side)
+		self.base = float(base)
+
+	def __repr__(self) -> str:
+		...
+
+	def __add__(self, other: 'Isosceles') -> 'Isosceles':
+		return Isosceles(
+			self.side + other.side,
+			self.base + other.base,
+		)
+
+	def __mul__(self, factor: float | int) -> 'Isosceles':
+		return Isosceles(
+			self.side * factor,
+			self.base * factor,
+		)
+
+	def __rmul__(self, factor: float | int) -> 'Isosceles':
+		return self * factor
+
+	@property
+	def perimeter(self) -> float:
+		return 2 * self.side + self.base
