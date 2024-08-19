7671d112d1e3d4ee8129e32fc15e2b333be0936f
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-08-19 09:31:25 +0000

    Attempt at making sequential structs abrstact

--- data_structures/sequential.py
+++ data_structures/sequential.py
@@ -1,22 +1,35 @@
 from __future__ import annotations
 
 
-class Node[Data]:
+import typing
+
+
+class SingleNode[Data]:
+
+	@classmethod
+	def insert(cls,
+		node: SingleNode[Data] | None,
+		data:            Data        ,
+	):
+		temp = SingleNode(
+			data = data,
+			next = node.next if node is not None else node
+		)
+
+		if node is not None:
+			node.next = temp
+
+	@classmethod
+	def delete(cls, node: SingleNode[Data]):
+		node.next = node.next.next if node.next is not None else node.next
+
 
 	def __init__(self,
-		data: Data,
-		next: Node | None = None,
-		prev: Node | None = None,
+		data:            Data               ,
+		next: SingleNode[Data] | None = None,
 	):
-		self._data = data
-		self._next = next
-		self._prev = prev
-
-		if next is not None:
-			next.prev = self
-
-		if prev is not None:
-			prev.next = self
+		self.data = data
+		self.next = next
 
 	def __next__(self):
 		if self.next is None:
@@ -26,51 +39,26 @@ class Node[Data]:
 
 
 	@property
-	def data(self) -> Data:
-		return self._data
-
-	@property
-	def next(self) -> Node | None:
+	def next(self) -> SingleNode[Data] | None:
 		return self._next
 
-	@property
-	def prev(self) -> Node | None:
-		return self._prev
-
-
 	@next.setter
-	def next(self, node: Node | None):
+	def next(self, node: SingleNode[Data] | None):
 		self._next = node
-
-		if node is not None:
-			node._prev = self
-
-	@prev.setter
-	def prev(self, node: Node | None):
-		self._prev = node
-
-		if node is not None:
-			node._next = self
-
 
 	@next.deleter
 	def next(self):
 		self.next = None
 
-	@prev.deleter
-	def prev(self):
-		self.prev = None
 
-
-class Deque[Data]:
+class List[Data]:
 
 	def __init__(self):
-		self.tail: Node | None = None
-		self.head: Node | None = None
-		self.node: Node | None = None
+		self.head: SingleNode[Data] | None = None
+		self.node: SingleNode[Data] | None = self.head
 
 	def __bool__(self) -> bool:
-		return self.tail is not None or self.head is not None
+		return self.head is not None
 
 	def __iter__(self):
 		self.node = self.head
@@ -86,63 +74,38 @@ class Deque[Data]:
 
 		return data
 
-
-	def append_tail(self, data: Data):
-		self.tail = Node(
-			data =      data,
-			prev = self.tail,
-		)
-
+	def pop(self) -> Data:
 		if self.head is None:
-			self.head = self.tail
+			raise IndexError(f"{self.pop.__name__}-ing from empty {self.__class__.__name__.lower()}")
 
-	def append_head(self, data: Data):
-		self.head = Node(
+		data      = self.head.data
+		self.head = self.head.next
+
+		return data
+
+
+class Stack[Data](List[Data]):
+
+	def push(self, data: Data):
+		self.head = SingleNode(
 			data =      data,
 			next = self.head,
 		)
 
-		if self.tail is None:
-			self.tail = self.head
 
-	def remove_tail(self) -> Data:
-		if self.tail is not None:
-			data      = self.tail.data
-			self.tail = self.tail.prev
-
-			if self.tail is None:
-				self.head = self.tail
-
-			return data
-
-		raise IndexError(f"pop from empty {self.__class__.__name__.lower()}")
-
-	def remove_head(self) -> Data:
-		if self.head is not None:
-			data      = self.head.data
-			self.head = self.head.next
-
-			if self.head is None:
-				self.tail = self.head
-
-			return data
-
-		raise IndexError(f"pop from empty {self.__class__.__name__.lower()}")
-
-
-class Stack[Data](Deque[Data]):
-
-	def push(self, data: Data):
-		return self.append_tail(data)
-
-	def pop(self) -> Data:
-		return self.remove_tail()
-
-
-class Queue[Data](Deque[Data]):
+class Queue[Data](List[Data]):
 
 	def enqueue(self, data: Data):
-		return self.append_tail(data)
+		SingleNode.insert(
+			data = data,
+			node = self.head,
+		)
 
 	def dequeue(self) -> Data:
-		return self.remove_tail()
+		if self.head is None:
+			raise IndexError(f"{self.pop.__name__}-ing from empty {self.__class__.__name__.lower()}")
+
+		data = self.head.data
+		SingleNode.delete(self.head)
+
+		return data
