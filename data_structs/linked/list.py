70ef0be49840bc4874073ab1e88257aad5b16d53
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-08-21 14:59:24 +0000

    Implement insertion methods for lists

--- data_structs/linked/list.py
+++ data_structs/linked/list.py
@@ -11,7 +11,25 @@ class Node[Data]:
 		self.next = next
 
 
+	def insert(self, data: Data):
+		self.next = Node(
+			data = data,
+			next = self.next,
+		)
+
+
+
 class List[Data]:
 
 	def __init__(self):
 		self.head: Node[Data] | None = None
+
+	def __bool__(self) -> bool:
+		return self.head is None
+
+
+	def append(self, data: Data):
+		self.head = Node(
+			data = data,
+			next = self.head,
+		)
