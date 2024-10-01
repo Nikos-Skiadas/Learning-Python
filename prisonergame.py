84976a5333d8a1ee78abaa29525ec67be6e7585e
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-10-01 14:46:39 +0000

    Make module nonexecutable when imported

--- prisonergame.py
+++ prisonergame.py
@@ -2,34 +2,32 @@
 
 # Prison Dilemma with tit for tat method
 def prison_dilemma():
-    
-
     algorithm_move = 'C'
-    
+
     actual_profit = 0
     maximum_profit = 0
-    
+
     print("Type 'C' for Cooperate or 'D' for Defect. Leave input empty to stop playing.")
-    
+
     while True:
         print(algorithm_move)
-        
+
         player_move = input().upper() # input to take player move and upper to capitalize
-        
+
         if not player_move:
             break
-        
+
         if player_move not in ['C', 'D']:
-            
+
             print("Invalid input. Please type 'C' or 'D'.")
             continue
-        
+
 		# Maximum profit
         if player_move == 'C':
             maximum_profit += 5
         else:
             maximum_profit += 1
-        
+
         if algorithm_move == 'C' and player_move == 'C':
             actual_profit += 3
         elif algorithm_move == 'C' and player_move == 'D':
@@ -38,11 +36,13 @@ def prison_dilemma():
             actual_profit += 5
         elif algorithm_move == 'D' and player_move == 'D':
             actual_profit += 1
-        
+
         algorithm_move = player_move
 
     print("\nGame over.")
     print(f"Maximum possible profit: {maximum_profit}")
     print(f"Actual profit: {actual_profit}")
 
-prison_dilemma()
+
+if __name__ == "__main__":
+	prison_dilemma()
