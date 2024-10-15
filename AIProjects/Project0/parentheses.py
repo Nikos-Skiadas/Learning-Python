dcbae8bb28d8e0c6baf60542f82834c2ea650561
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-10-15 15:53:06 +0000

    Make solution pretty

--- AIProjects/Project0/parentheses.py
+++ AIProjects/Project0/parentheses.py
@@ -1,11 +1,9 @@
 def complete_parentheses(expression):
-    
-    """
-    This function takes a list representing a numerical expression without left parentheses
+    """This function takes a list representing a numerical expression without left parentheses
     and returns the expression with the correct left parentheses inserted.
     """
     stack = []
-    
+
     for token in expression:
         if token == ')':
             operand2 = stack.pop()
@@ -14,26 +12,29 @@ def complete_parentheses(expression):
             stack.append(['(', operand1, operator, operand2, ')'])
         else:
             stack.append(token)
-    
+
     return stack[0]
+
 
 def flatten_expression(expr):
 
     flat = []
+
     for item in expr:
         if isinstance(item, list):
             flat.extend(flatten_expression(item))  # Recursively flatten nested lists
+
         else:
             flat.append(item)
-            
+
     return flat
 
-if __name__ == "__main__":
-    
-    expression = [1, '-', 2, ')', '*', 3, '-', 4, ')', '*', 5, '-', 6, ')', ')', ')']
-    result = complete_parentheses(expression)
-    
-    flattened_result = flatten_expression(result)
-    
-    print(*flattened_result)
 
+if __name__ == "__main__":
+
+    expression = [1, '-', 2, ')', '*', 3, '-', 4, ')', '*', 5, '-', 6, ')', ')', ')', ')']
+    result = complete_parentheses(expression)
+
+    flattened_result = flatten_expression(result)
+
+    print(*flattened_result)
