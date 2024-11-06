7cd04884cf9a3f9ced9a484a078cbd9498633910
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-11-06 06:46:04 +0000

    Serialize minimax game

--- AIProjects/Project2/minimax.py
+++ AIProjects/Project2/minimax.py
@@ -8,23 +8,17 @@ import math
 import typing
 
 
+class Game[State]:
 
-class Action:
-
-	...
-
-
-class State:
-
-	def __init__(self, node: str, *children: State,
+	def __init__(self, state: State, *children: Game,
 		terminal_utility: float = 0,
 	):
-		self.name = node
+		self.state = state
 		self.children = list(children)
 		self.terminal_utility = terminal_utility
 
 	@classmethod
-	def max(cls, state: State) -> float:
+	def max(cls, state: Game) -> float:
 		if state.terminal:
 			return state.utility
 
@@ -32,7 +26,7 @@ class State:
 
 
 	@classmethod
-	def min(cls, state: State) -> float:
+	def min(cls, state: Game) -> float:
 		if state.terminal:
 			return state.utility
 
@@ -40,12 +34,12 @@ class State:
 
 
 	@property
-	def minmax(self) -> State | None:
+	def minmax(self) -> Game | None:
 		if self.terminal:
 			return None
 
 		return max(self.children,
-			key = State.min,
+			key = Game.min,
 		)
 
 	@property
@@ -54,64 +48,76 @@ class State:
 
 	@property
 	def utility(self) -> float:
-		return self.terminal_utility if self.terminal else State.max(self)
+		return self.terminal_utility if self.terminal else Game.max(self)
+
+
+	@classmethod
+	def from_json(cls, start: State, next: dict | float) -> Game:
+		if isinstance(next, dict):
+			return Game(start, *(Game.from_json(turn, children) for turn, children in next.items()))
+
+		return Game(start,
+			terminal_utility = next
+		)
+
+
 
 	@property
 	def json(self) -> dict:
-		dict = {}
+		dictionary = {}
 
 		if self.terminal:
-			dict[self.name] = self.utility
+			dictionary[self.state] = self.utility
 
 		else:
 			for state in self.children:
-				dict[state.name] = state.json
+				dictionary[state.state] = state.json
 
-		return dict
+		return dictionary
 
 
 
 if __name__ == "__main__":
-	game = State("a",
-		State("aa",
-			State("aaa",
-				State("aaaa", terminal_utility = +4),
-				State("aaab", terminal_utility = +8),
-			),
-			State("aab",
-				State("aaba", terminal_utility = +9),
-				State("aabb", terminal_utility = +3),
-			),
-		),
-		State("ab",
-			State("aba",
-				State("abaa", terminal_utility = +2),
-				State("abab", terminal_utility = -2),
-			),
-			State("abb",
-				State("abba", terminal_utility = +9),
-				State("abbb", terminal_utility = -1),
-			),
-			State("abc",
-				State("abca", terminal_utility = +8),
-				State("abcb", terminal_utility = +4),
-			),
-		),
-		State("ac",
-			State("aca",
-				State("acaa", terminal_utility = +3),
-				State("acab", terminal_utility = +6),
-				State("acac", terminal_utility = +5),
-			),
-			State("acb",
-				State("acba", terminal_utility = +7),
-				State("acbb", terminal_utility = +1),
-			),
-		),
-	)
+	states = {
+		"aa": {
+			"aaa": {
+				"aaaa": +4,
+				"aaab": +8,
+			},
+			"aab": {
+				"aaba": +9,
+				"aabb": +3,
+			},
+		},
+		"ab": {
+			"aba": {
+				"abaa": +2,
+				"abab": -2,
+			},
+			"abb": {
+				"abba": +9,
+				"abbb": -1,
+			},
+			"abc": {
+				"abca": +8,
+				"abcb": +4,
+			},
+		},
+		"ac": {
+			"aca": {
+				"acaa": +3,
+				"acab": +6,
+				"acac": +5,
+			},
+			"acb": {
+				"acba": +7,
+				"acbb": +1,
+			},
+		},
+	}
 
 	print(
-		json.dumps(game.json,
+		json.dumps(Game.from_json("a", states).json,
 			indent = 4,
 		)
 	)
