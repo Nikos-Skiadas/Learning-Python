fbb810d725fee8020a820710b3181649e97d0cde
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-02-08 14:49:56 +0000

    Start implementing squares

--- chess/__init__.py
+++ chess/__init__.py
@@ -9,22 +9,36 @@ class Game:
 	...
 
 
+class Square(tuple[int, int]):
+
+	def __repr__(self) -> str:
+		...
+
+
+	@property
+	def rank(self) -> int:
+		return self[0] + 1
+
+	@property
+	def file(self) -> str:
+		return chr(self[1] + ord("a"))
+
+	@property
+	def black(self) -> bool:
+		return (self[0] + self[1]) % 2 == 0
+
+
+class Board(list[Square]):
+
+	...
+
+
 class Player:
 
 	...
 
 
-class Board:
-
-	...
-
-
 class Piece:
-
-	...
-
-
-class Square:
 
 	...
 
@@ -37,4 +51,3 @@ class History:
 class Move:
 
 	...
-
