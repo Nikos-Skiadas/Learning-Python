8f88ec44889a30a56b1407f8124d1ea3e056dbd5
Nikos <nikos.skiadasa2@gmail.com>
2025-02-08 16:22:49 +0200

    Initial commit

---
+++ chess/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+
+import typing
+
+
+class Game:
+
+	...
+
+
+class Player:
+
+	...
+
+
+class Board:
+
+	...
+
+
+class Piece:
+
+	...
+
+
+class Square:
+
+	...
+
+
+class History:
+
+	...
+
+
+class Move:
+
+	...
+
