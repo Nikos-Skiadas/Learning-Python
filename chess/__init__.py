39e159183e1c849e3d0bb6dcbffe1ebd34b020ae
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-02-11 14:58:47 +0000

    Add `__add__`

--- chess/__init__.py
+++ chess/__init__.py
@@ -6,22 +6,37 @@ import typing
 
 class Square(tuple[int, int]):
 
-	def __new__(cls, notation: str) -> Square:
-		file, rank = notation
+	def __new__(cls,
+		file: int,
+		rank: int,
+	) -> Square:
+		square = super().__new__(cls, (file, rank))
 
-		if not 'a' <= file <= 'h' or not '1' <= rank <= '8':
-			raise IndexError(f"Invalid square notation: {notation}")
+		# Check if tuple of integers is within chess bounds:
+		if not "a" <= square.file <= "h" or not "1" <= square.rank <= "8":
+			raise IndexError(f"Invalid square {square}")
 
-		return super().__new__(cls, (int(rank) - 1, ord(file) - ord("a")))
+		return square
+
 
 
 	def __repr__(self) -> str:
 		return self.file + str(self.rank)
 
 
+	@classmethod
+	def fromnotation(cls, notation: str) -> Square:
+		file, rank = notation
+
+		if not "a" <= file <= "h" or not "1" <= rank <= "8":
+			raise IndexError(f"Invalid square notation: {notation}")
+
+		return super().__new__(cls, (int(rank) - 1, ord(file) - ord("a")))
+
+
 	@property
-	def rank(self) -> int:
-		return self[0] + 1
+	def rank(self) -> str:
+		return str(self[0] + 1)
 
 	@property
 	def file(self) -> str:
@@ -32,11 +47,26 @@ class Square(tuple[int, int]):
 		return (self[0] + self[1]) % 2 == 0
 
 
-	def __sub__(self, other: Square) -> tuple[int, int]:
-		return (self[0] - other[0], self[1] - other[1])
+	def __add__(self, other: Vector) -> Square:
+		return Square(
+			self[0] + other[0],
+			self[1] + other[1],
+		)
+
+	def __sub__(self, other: Square) -> Vector:
+		return Vector(
+			self[0] - other[0],
+			self[1] - other[1],
+		)
 
 
+class Vector(tuple[int, int]):
 
+	def __new__(cls,
+		file_diff: int,
+		rank_diff: int,
+	) -> Vector:
+		return super().__new__(cls, (file_diff, rank_diff))
 
 
 
