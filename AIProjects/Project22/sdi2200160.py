b9c7a2e1590d5dcbdbb17a86f2dda073c6a83ea7
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-04-10 18:05:14 +0000

    First training attempt

--- AIProjects/Project22/sdi2200160.py
+++ AIProjects/Project22/sdi2200160.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 import warnings; warnings.simplefilter(action = "ignore", category = UserWarning)
 
-import os
+import math
+from collections import Counter
+import os; os.environ["TORCH_INDUCTOR_MAX_AUTOTUNE_GEMM"] = "0"  # disable autotuning
 from pathlib import Path
 import re
 import string
 from typing import Callable, Iterable, Literal, Self
 
-from rich.progress import track
+from rich.progress import Progress, track
 import pandas
 import torch; torch.set_default_device("cuda")
 
@@ -136,18 +138,40 @@ class Embedding(torch.nn.Embedding):
 	@classmethod
 	def from_glove(cls,
 		embedding_dim: Literal[50, 100, 200, 300] = 50,
-	**kwargs) -> Self:
+		pad_token: str = "<pad>",
+		unk_token: str = "<unk>",
+		**kwargs,
+	) -> Self:
 		source_path: Path = Path("embeddings") / f"glove.6B.{embedding_dim}d.txt"
 		target_path: Path = source_path.with_suffix(".word2vec.txt")
 
-		cls.convert_glove_to_word2vec(
-			source_path,
-			target_path,
-		)
+		cls.convert_glove_to_word2vec(source_path, target_path)
+
 		word2idx, tensor = cls.load_word2vec_format(target_path)
 
+		# Insert special tokens:
+		pad_vector = torch.zeros(tensor.shape[1], device=tensor.device)
+	#	unk_vector = torch.randn(tensor.shape[1], device=tensor.device) * 0.1  # smaller variance
+
+		# Rebuild mapping with special tokens:
+		word2idx = {
+			pad_token: 0,
+		#	unk_token: 1,
+		**{word: idx + 2 for word, idx in word2idx.items()}}
+
+		# Stack special vectors:
+		tensor = torch.vstack(
+			[
+				pad_vector,
+			#	unk_vector,
+			tensor]
+		)
+
 		self = cls.from_pretrained(tensor, **kwargs)
-		self.word2idx = word2idx
+		self.word2idx = Vocabulary(word2idx,
+			pad_token = pad_token,
+		#	unk_token = unk_token,
+		)
 
 		return self
 
@@ -164,7 +188,7 @@ class Embedding(torch.nn.Embedding):
 		with open(target_path if target_path is not None else source_path, "w+", encoding="utf-8") as target_file:
 			target_file.write(f"{num_tokens} {embedding_dim}\n")  # write the `word2vec` header
 
-			for line in track(lines, "converting glove to word2vec", num_tokens):
+			for line in track(lines, "converting glove to word2vec".ljust(32), num_tokens):
 				target_file.write(line)  # copy the rest of the lines
 
 	@classmethod
@@ -175,7 +199,7 @@ class Embedding(torch.nn.Embedding):
 			word2idx = {}  # word to index mapping
 			vectors = torch.zeros(num_tokens, embedding_dim)  # preallocate tensor for word vectors
 
-			for index, line in track(enumerate(word2vec_file), "get embeddings from word2vec", num_tokens):
+			for index, line in track(enumerate(word2vec_file), "get embeddings from word2vec".ljust(32), num_tokens):
 				word, *vec = line.strip().split()  # split word and vector
 				vec = torch.tensor(list(map(float, vec)))  # convert vector to tensor
 
@@ -241,36 +265,104 @@ class TwitterClassifier:
 		learning_rate: float = 1e-3,
 		weight_decay: float = 0.,
 	):
+		def accuracy (x: torch.Tensor, y: torch.Tensor) -> torch.Tensor: return (x * y).float().mean()
+		def precision(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor: return (x * y).float().sum() / x.sum()
+		def recall   (x: torch.Tensor, y: torch.Tensor) -> torch.Tensor: return (x * y).float().sum() / y.sum()
+
+		def f1       (x: torch.Tensor, y: torch.Tensor) -> torch.Tensor: return 2 / (1 / precision(x, y) + 1 / recall(x, y))
+
 		self.optimizer = torch.optim.AdamW(self.model.parameters(),
 			lr = learning_rate,
 			weight_decay = weight_decay,
 		)
 		self.loss_fn = torch.nn.BCEWithLogitsLoss()
+		self.metrics = {
+			"loss"     : self.loss_fn,
+			"accuracy" : accuracy,
+			"precision": precision,
+			"recall"   : recall,
+			"f1"       : f1,
+		}
 
-	def fit(self, dataset: TwitterDataset,
-		epochs: int = 5,
-	**kwargs) -> float:
-		loader = torch.utils.data.DataLoader(dataset,
-			drop_last = True,
-		**kwargs)
+		return self
 
-		for _ in range(1, epochs + 1):
-			self.model.train()
+	def compute(self,
+		y_pred: torch.Tensor,
+		y_true: torch.Tensor,
+	) -> dict[str, float]:
+		y_prob = torch.sigmoid(y_pred)
 
-			for batch in track(loader, "batch", len(dataset) // loader.batch_size if loader.batch_size is not None else None):
-				x, y_true = batch
+		return {name: metric(y_pred if name == "loss" else y_prob, y_true).item() for name, metric in self.metrics.items()}
 
-				self.optimizer.zero_grad()
-				y_pred = self.model(x)
-				loss = self.loss_fn(y_pred, y_true)
-				loss.backward()
-				self.optimizer.step()
 
-		return loss.item()
+	def fit(self,
+		train_dataset: TwitterDataset,
+		val_dataset  : TwitterDataset,
+		epochs: int = 1,
+	**kwargs) -> dict[str, list[float]]:
+		train_loader = torch.utils.data.DataLoader(train_dataset, drop_last = True, **kwargs)
+		val_loader   = torch.utils.data.DataLoader(val_dataset  , drop_last = True, **kwargs)
+
+		metrics = Counter(
+			loss      = [], val_loss      = [],  # type: ignore
+			accuracy  = [], val_accuracy  = [],  # type: ignore
+			precision = [], val_precision = [],  # type: ignore
+			recall    = [], val_recall    = [],  # type: ignore
+			f1        = [], val_f1        = [],  # type: ignore
+		)
+
+		with Progress() as progress:
+			train_task = progress.add_task(
+				description = "training epoch --/--".ljust(32),
+				total = epochs
+			)
+			batch_task = progress.add_task(
+				description = "training loss -.----".ljust(32),
+				total = len(train_dataset) // train_loader.batch_size if train_loader.batch_size is not None else None
+			)
+
+			for epoch in range(1, epochs + 1):
+				self.model.train()
+
+				for batch in train_loader:
+					x, y_true = batch
+
+					self.optimizer.zero_grad()
+					y_pred = self.model.forward(x)
+					loss = self.loss_fn.forward(
+						y_pred,
+						y_true,
+					)
+					loss.backward()
+					self.optimizer.step()
+
+					progress.update(batch_task,
+						description = f"training loss {loss.item():.4f}".ljust(32),
+						advance = 1,
+					)
+
+				epoch_metrics = self.compute(
+					y_pred,
+					y_true,
+				)
+				metrics.update({       name  : [metric] for name, metric in              epoch_metrics.items()})
+				metrics.update({f"val_{name}": [metric] for name, metric in self.evaluate(val_dataset).items()})
+
+				progress.update(batch_task,
+					completed = 0,
+				)
+				progress.update(train_task,
+					description = f"training epoch {epoch:02d}/{epochs:02d}".ljust(32),
+					advance = 1,
+				)
+
+		return dict(metrics)  # type: ignore
 
 	@torch.no_grad
-	def evaluate(self, dataset: TwitterDataset, **kwargs) -> float:
-		loader = torch.utils.data.DataLoader(dataset,
+	def evaluate(self, test_dataset: TwitterDataset, **kwargs) -> dict[str, float]:
+		batch_size = kwargs.pop("batch_size", len(test_dataset))
+		loader = torch.utils.data.DataLoader(test_dataset,
+			batch_size = batch_size,  # evaluate on the whole dataset
 			drop_last = True,
 		**kwargs)
 
@@ -280,9 +372,12 @@ class TwitterClassifier:
 			x, y_true = batch
 
 			y_pred = self.model(x)
-			loss = self.loss_fn(y_pred, y_true)
+			metrics = self.compute(
+				y_pred,
+				y_true,
+			)
 
-		return loss.item()
+		return metrics
 
 	@torch.no_grad
 	def predict(self, dataset: TwitterDataset, **kwargs) -> torch.Tensor:
@@ -305,17 +400,26 @@ if __name__ == "__main__":
 	embedding = Embedding.from_glove(50)
 	model = TwitterModel(embedding)
 	model.compile()
-	classifier = TwitterClassifier(model)
 
-	train_data = TwitterDataset("train",
-		transform = TextTransform(embedding.word2idx,
-			preprocessor = Preprocessor(),
-			tokenizer = Tokenizer(),
-			max_len = 32,
-		)
+	classifier = TwitterClassifier(model)
+	classifier.compile()
+
+	transform = TextTransform(embedding.word2idx,
+		preprocessor = Preprocessor(),
+		tokenizer = Tokenizer(),
+		max_len = 32,
 	)
 
-	classifier.fit(train_data,
-		epochs = 1,
-		batch_size = 32,
+	train_data = TwitterDataset("train",
+		transform = transform
+	)
+	val_data = TwitterDataset("val",
+		transform = transform
+	)
+
+	history = classifier.fit(
+		train_data,
+		val_data,
+		epochs = 10,
+		batch_size = math.isqrt(len(train_data)) + 1,
 	)
