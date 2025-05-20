193d4b0f12519933a1da83417186a3ace702db7e
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-05-20 16:56:37 +0000

    Modularize code for testing

--- AIProjects/Project23/sdi2200160.py
+++ AIProjects/Project23/sdi2200160.py
@@ -15,54 +15,9 @@ import evaluate
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import torch; torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
+import torch; device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import datasets
 import transformers
-
-
-class BertSentenceEmbedder:
-
-	def __init__(self, *,
-		model_name = "bert-base-uncased",  # Default to the standard uncased BERT
-	):
-		self.tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
-		self.model: transformers.BertModel = transformers.BertModel.from_pretrained(model_name,
-			output_hidden_states = True,
-		)
-		self.model = self.model.to(self.model,
-			device = torch.get_default_device())
-		self.model.eval()  # Evaluation mode
-
-	def __call__(self, texts: list[str] | str) -> torch.Tensor:
-		return self.encode(texts)
-
-
-	def encode(self, texts: list[str] | str) -> torch.Tensor:
-		if isinstance(texts, str):
-			texts = [texts]
-
-		# Tokenize text input
-		tokens = self.tokenizer(texts,
-			padding = True,  # Pad sequences to the same length
-			truncation = True,  # Truncate if too long for BERT
-			return_tensors = "pt",  # Return as PyTorch tensors
-		)
-
-		# Inference without gradients
-		with torch.no_grad():
-			output = self.model(**tokens)
-			hidden_states = output.hidden_states  # (layers, batch, tokens, hidden_dim)
-
-		token_embeddings = torch.stack(hidden_states,
-			dim = 0,  # Stack to shape: (layers, batch, tokens, dim)
-		)
-
-		# Return sentence embedding: (batch, dim) — one vector per input text
-		return token_embeddings.sum(
-			 dim = 0,  # Sum across layers
-		).mean(
-			 dim = 1,  # Mean across tokens
-		)
 
 
 def fix_seed(seed: int = 42):
@@ -78,114 +33,194 @@ def fix_seed(seed: int = 42):
 	torch.backends.cudnn.benchmark = False
 
 
-fix_seed()
 
+class TwitterDataset(datasets.DatasetDict):
 
-def twitter_dataset(
-	model_name: str = "bert-base-uncased",
-	root: Path = Path().cwd(),
-**column_types: datasets.Value):
-	if not column_types:
-		column_types = dict(
-			index  = datasets.Value(dtype = "int32" ),
-			text   = datasets.Value(dtype = "string"),
-			labels = datasets.Value(dtype = "int32" ),
+	@classmethod
+	def preprocessed(cls,
+		model_name: str = "bert-base-uncased",
+		root: Path = Path().cwd(),
+	**column_types: datasets.Value):
+
+		if not column_types:
+			column_types = dict(
+				index  = datasets.Value(dtype = "int32" ),
+				text   = datasets.Value(dtype = "string"),
+				labels = datasets.Value(dtype = "int32" ),
+			)
+
+		features = datasets.Features(column_types)
+
+		logging.info("Loading dataset...")
+
+		dataset = cast(datasets.DatasetDict,
+			datasets.load_dataset("csv",
+				name = "Twitter",
+				data_files = dict(
+					train = str(root / "train_dataset.csv"),
+					val   = str(root /   "val_dataset.csv"),
+					test  = str(root /  "test_dataset.csv"),
+				),
+			)
 		)
 
-	features = datasets.Features(column_types)
+		logging.info("Dataset loaded.")
+		logging.info("Renaming columns...")
 
-	dataset = cast(datasets.DatasetDict,
-		datasets.load_dataset("csv",
-			name = "Twitter",
-			data_files = dict(
-				train = str(root / "train_dataset.csv"),
-				val   = str(root / "val_dataset.csv"  ),
-				test  = str(root / "test_dataset.csv" ),
-			),
+		for split in dataset:
+			columns = dict(zip(dataset[split].column_names, column_types))
+			dataset[split] = dataset[split].rename_columns(columns).cast(features).select(range(64))
+
+		logging.info("Columns renamed.")
+		logging.info("Processing dataset...")
+
+		tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
+
+		def tokenize(batch):
+			return tokenizer(batch["text"],
+				padding = "max_length",
+				truncation = True,
+			)
+
+		dataset = dataset.map(tokenize)
+		dataset.set_format(
+			type = "torch",
+			columns = [
+				"input_ids",
+				"attention_mask",
+				"labels",
+			],
 		)
-	)
+		dataset["test"] = dataset["test"].remove_columns("labels")
 
-	for split in dataset:
-		columns = dict(zip(dataset[split].column_names, column_types))
-		dataset[split] = dataset[split].rename_columns(columns).cast(features)
+		logging.info("Dataset processed.")
 
-#	tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
-
-#	def tokenize(batch):
-#		return tokenizer(batch["text"],
-#			padding = "max_length",
-#			truncation = True,
-#		)
-
-#	dataset = dataset.map(tokenize)
-#	dataset.set_format(
-#		type = "torch",
-#		columns = [
-#			"input_ids",
-#			"attention_mask",
-#			"labels",
-#		],
-#	)
-
-	dataset["test"] = dataset["test"].remove_columns("labels")
-
-	return dataset
-
-model_name = "bert-base-uncased"
-model = transformers.BertForSequenceClassification.from_pretrained(model_name,
-	num_labels = 2,
-)
-
-training_args = transformers.TrainingArguments(
-	output_dir = "./results",
-	logging_dir = "./logs",
-
-	evaluation_strategy = "epoch",
-	save_strategy = "epoch",
-
-#	per_device_train_batch_size = 16,
-#	per_device_eval_batch_size = 64,
-
-	num_train_epochs = 4,
-	learning_rate = 1e-4,
-	weight_decay = 1e-2,
-
-	load_best_model_at_end = True,
-#	metric_for_best_model = "accuracy",  # `eval_loss` by default
-)
+		return cls(dataset)
 
 
-dataset = twitter_dataset()
+class TwitterClassifier:
+
+	def __init__(self,
+		model_name: str | Path = "bert-base-uncased",
+		num_labels: int = 2,
+	) -> None:
+		self.model_name = model_name
+		self.num_labels = num_labels
+
+		self.tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
+		self.model = transformers.BertForSequenceClassification.from_pretrained(model_name,
+			num_labels = num_labels,
+		)
 
 
-def compute_metrics(eval_pred: transformers.EvalPrediction) -> dict[str, float]:
-	metrics = evaluate.combine(
-		[
-			evaluate.load("accuracy"                     ),
-			evaluate.load("precision", average = "binary"),
-			evaluate.load("recall"   , average = "binary"),
-			evaluate.load("f1"       , average = "binary"),
-		]
-	)
+	@classmethod
+	def load(cls, path: Path):
+		return cls(path)
 
-	y_pred, y_true = eval_pred
-	y_pred = np.argmax(y_pred,
-		axis = 1,
-	)
-
-	return metrics.compute(
-		predictions = y_pred,
-		references  = y_true,
-	)
+	def save(self, path: Path):
+		self.model.save_pretrained(path)
+	#	self.tokenizer.save_pretrained(path)
 
 
-trainer = transformers.Trainer(
-	model = model,
-	args = training_args,
-	train_dataset = dataset["train"],
-	eval_dataset = dataset["val"],
-	processing_class = transformers.BertTokenizer.from_pretrained(model_name),
-	compute_metrics = compute_metrics,
-)
+	def compile(self,
+		training_args: transformers.TrainingArguments = transformers.TrainingArguments(
+			output_dir = "./results",
+			logging_dir = "./logs",
 
-trainer.train()
+			eval_strategy = "epoch",
+			save_strategy = "epoch",
+
+			per_device_train_batch_size = 32,
+			per_device_eval_batch_size = 128,
+			fp16 = True,
+
+			num_train_epochs = 4,
+			learning_rate = 1e-4,
+			weight_decay = 1e-2,
+
+			load_best_model_at_end = True,
+		#	metric_for_best_model = "accuracy",  # `eval_loss` by default
+		)
+	):
+		self.trainer = transformers.Trainer(
+			model = self.model,
+			args = training_args,
+			processing_class = self.tokenizer,
+			compute_metrics = self.compute_metrics,
+		)
+
+	def fit(self, dataset: TwitterDataset) -> dict[str, float]:
+		self.trainer.train_dataset = dataset["train"]
+		self.trainer.eval_dataset  = dataset["val"]
+
+		self.model.train()
+		output = self.trainer.train()
+		self.save(Path.cwd() / "model")
+
+		return output.metrics
+
+	def evaluate(self, dataset: TwitterDataset) -> dict[str, float]:
+		self.trainer.eval_dataset = dataset["val"]
+
+		self.model.eval()
+
+		return self.trainer.evaluate()
+
+
+	def predict(self, texts: list[str] | str) -> list[int]:
+		return torch.argmax(self.logits(texts),
+			dim = 1,
+		).tolist()
+
+	def predict_proba(self, texts: list[str] | str) -> list[float]:
+		return torch.softmax(self.logits(texts),
+			dim = 1,
+		)[:, 1].tolist()
+
+
+	def logits(self, texts: list[str] | str) -> torch.Tensor:
+		if isinstance(texts, str):
+			texts = [texts]
+
+		tokens = self.tokenizer(texts,
+			return_tensors = "pt",
+			padding = True,
+			truncation = True,
+		)
+		tokens = {k: v.to(device) for k, v in tokens.items()}
+
+		with torch.no_grad():
+			logits = self.model(**tokens).logits
+
+			return logits.cpu()
+
+	@staticmethod
+	def compute_metrics(eval_pred: transformers.EvalPrediction) -> dict[str, float]:
+		metrics = evaluate.combine(
+			[
+				evaluate.load("accuracy"                     ),
+				evaluate.load("precision", average = "binary"),
+				evaluate.load("recall"   , average = "binary"),
+				evaluate.load("f1"       , average = "binary"),
+			]
+		)
+
+		y_pred, y_true = eval_pred
+		y_pred = np.argmax(y_pred,
+			axis = 1,
+		)
+
+		return metrics.compute(
+			predictions = y_pred,
+			references  = y_true,
+		)
+
+
+if __name__ == "__main__":
+	fix_seed()
+
+	dataset = TwitterDataset.preprocessed()
+	classifier = TwitterClassifier()
+	classifier.compile()
+	classifier.fit(dataset)
+	classifier.evaluate(dataset)
