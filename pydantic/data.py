c1563a0f8e570b435802463234f7765733545c54
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-05-22 17:38:19 +0000

    Person dataclass

--- pydantic/data.py
+++ pydantic/data.py
@@ -1,19 +1,24 @@
 from __future__ import annotations
 
 
-from datetime import datetime, timedelta
-from typing import Self
+from datetime import datetime
+from dataclasses import dataclass
+from typing import ClassVar, Self
 
 
 class Person:
 
-	count = 0
+	count: int = 0
 
 
-	def __init__(self, name: str, birthday: str):
+	def __init__(self, name: str, birthday: datetime | str) -> None:
+		if isinstance(birthday, str):
+			birthday = datetime.fromisoformat(birthday)
+
 		self.name = name
-		self.birthday = datetime.fromisoformat(birthday)
-		self.friends = set()
+		self.birthday = birthday
+
+		self.friends: set[Self] = set()
 
 		self.__class__.count += 1
 
@@ -32,7 +37,7 @@ class Person:
 
 	@classmethod
 	def born(cls, name: str) -> Self:
-		return cls(name, datetime.now().isoformat())
+		return cls(name, datetime.now())
 
 	@property
 	def age(self) -> int:
@@ -41,6 +46,48 @@ class Person:
 	def greet(self) -> str:
 		return f"Hello, my name is {self.name} and I am {self.age} years old."
 
-	def add(self, other: Person) -> None:
+	def add(self, other: Self) -> None:
+		self.friends.add(other)
+		other.friends.add(self)
+
+
+@dataclass
+class PersonData:
+
+	count: ClassVar[int] = 0
+
+	name: str
+	birthday: datetime
+
+
+	def __post_init__(self):
+		self.__class__.count += 1
+		self.friends: set[Self]  = set()
+
+		if isinstance(self.birthday, str):
+			self.birthday = datetime.fromisoformat(self.birthday)
+
+	def __del__(self):
+		self.__class__.count -= 1
+
+	def __hash__(self):
+		return hash(self.name)
+
+	def __eq__(self, other: Self) -> bool:
+		return self.name == other.name
+
+
+	@classmethod
+	def born(cls, name: str) -> Self:
+		return cls(name, datetime.now())
+
+	@property
+	def age(self) -> int:
+		return (datetime.now() - self.birthday).days // 365
+
+	def greet(self) -> str:
+		return f"Hello, my name is {self.name} and I am {self.age} years old."
+
+	def add(self, other: Self) -> None:
 		self.friends.add(other)
 		other.friends.add(self)
