dbb99b1c9cf327cfe2e43c55b672f988ef64f1aa
Nikos-Skiadas <nikos.skiadasa2@gmail.com>
2026-04-02 18:11:41 +0300

    Make changes

--- data-mining/project/src/data.py
+++ data-mining/project/src/data.py
@@ -10,6 +10,12 @@ import typing
 type BinaryDataFrame = pandas.DataFrame
 
 
+def normalize_ids(index: pandas.Index) -> pandas.Index:
+	return pandas.Index(index.map(
+		lambda song_id: str(song_id).strip(),
+	))
+
+
 class MusicSeries(pandas.Series):
 
 	@classmethod
@@ -46,7 +52,7 @@ class MusicSeries(pandas.Series):
 		).head(k).index
 
 	def top(self, k: int) -> pandas.Series:
-		return self[self.top_labels(k)]
+		return self[self.mask(self.top_labels(k))]
 
 
 class MusicDataFrame(pandas.DataFrame):
@@ -67,6 +73,11 @@ def intersection(
 	lyrics: MusicSeries,
 	genres: MusicSeries,
 ) -> MusicDataFrame:
+	# Keep the join key consistent even if one file loads ids as ints and another as strings.
+	audio_stats.index = normalize_ids(audio_stats.index)
+	lyrics.index = normalize_ids(lyrics.index)
+	genres.index = normalize_ids(genres.index)
+
 	ids = audio_stats.index \
 		.intersection(lyrics.index, sort = False) \
 		.intersection(genres.index, sort = False)
@@ -86,11 +97,18 @@ def intersection(
 	lyrics = lyrics.loc[mask]
 	genres = genres.loc[mask]
 
+	# If audio stats are wide, store each row as one MFCC vector. If they are already one column, keep that value.
+	mfcc_stats = (
+		audio_stats.iloc[:, 0].to_numpy()
+		if audio_stats.shape[1] == 1
+		else audio_stats.to_numpy().tolist()
+	)
+
 	return MusicDataFrame(
 		{
 			"id": audio_stats.index,
 			"genre": genres.to_numpy(),
 			"lyrics": lyrics.to_numpy(),
-			"mfcc_stats": audio_stats.to_numpy().tolist(),
+			"mfcc_stats": mfcc_stats,
 		}
 	).drop_duplicates(subset = "id").reset_index(drop = True)
