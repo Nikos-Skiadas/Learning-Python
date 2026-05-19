7fe2076dc98c12bb2cc8a28af006fcaca9752b21
Stratos Papadoudis <stratos.papadoudis@helvia.ai>
2026-05-19 22:00:42 +0300

    Annotate execution order

--- artificial_intelligence/homeworks/hw20260521/__init__.py
+++ artificial_intelligence/homeworks/hw20260521/__init__.py
@@ -1,2 +1,3 @@
-# python -m artificial_intelligence.homeworks.hw20260521.sdi2200160 --preset full
-# python -m artificial_intelligence.homeworks.hw20260521.sdi2200160 --help  # for help
+# python -m artificial_intelligence.homeworks.hw20260521.sdi2200160 --help          # NOTE: for help
+# python -m artificial_intelligence.homeworks.hw20260521.sdi2200160 --preset check  # NOTE: check everything works
+# python -m artificial_intelligence.homeworks.hw20260521.sdi2200160 --preset full   # NOTE: run the darn thing
